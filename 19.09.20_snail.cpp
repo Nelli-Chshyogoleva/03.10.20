d864cfdf952e99464a7e0be5dc3d79bd91d37e1f
Nelli-Chshyogoleva <72495610+Nelli-Chshyogoleva@users.noreply.github.com>
2020-12-03 13:09:47 +0600

    Create 19.09.20_snail.cpp

---
+++ 19.09.20_snail.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+// R = rows = строки
+// С = cols = столбцы
+// T = top = верх
+// B = bottom = низ
+// a = координата
+// b = координата
+int main()
+{
+    const int R = 6, C = 7;
+    int N[R][C];
+    int k = 1, a = 0, b = 0, LT = 0, RT = C - 1, T = 1, B = R - 1;
+    do {
+        do {
+            N[b][a] = k;
+            ++k;
+            ++a;
+        } while (a < RT);
+        --RT;
+        if (k <= R * C) {
+            do {
+                N[b][a] = k;
+                ++k;
+                ++b;
+            } while (b < B);
+            --B;
+        }
+        if (k <= R * C) {
+            do {
+                N[b][a] = k;
+                ++k;
+                --a;
+            } while (a > LT);
+            ++LT;
+        }
+        if (k <= R * C) {
+            do {
+                N[b][a] = k;
+                ++k;
+                --b;
+            } while (b > T);
+            ++T;
+        }
+
+    } while (k <= R * C);
+    for (int i = 0; i < R; ++i) {
+        for (int j = 0; j < C; ++j) {
+            std::cout << N[i][j] << ' ';
+        }
+        std::cout << "\n";
+    }
+}
