7dfd790a40fa9230bd8a6d0942bb3c62cb9dce3b
Nelli-Chshyogoleva <72495610+Nelli-Chshyogoleva@users.noreply.github.com>
2020-12-11 15:11:37 +0600

    Update 19.09.20_alignment.cpp

--- 19.09.20_alignment.cpp
+++ 19.09.20_alignment.cpp
@@ -1,25 +1,65 @@
 #include <iostream>
-#include <iomanip>
 
 int main()
 {
 	const int n = 5, m = 6;
-	int N[n][m];
+	int M[n][m];
 	for (int i = 0; i < n; ++i)
 		for (int j = 0; j < m; ++j)
-			N[i][j] = rand() % 1000;
+			M[i][j] = rand()%100000;
+	int* w = new int[m];
+	for (int j = 0; j < m; ++j) {
+		w[j] = 0;
+		for (int i = 0; i < n; ++i) {
+			int d_m = 0;
+			int n = M[i][j];
+			do {
+				++d_m;
+				n /= 10;
+			} while (n > 0);
+			
+			if (w[j] < d_m)
+				w[j] = d_m;
+		}
+	}
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
 
-	for (int i = 0; i < n; ++i){
-		for (int j = 0; j < m; ++j){
-			std::cout << std::setw(8) << N[i][j];
+			int d_m = 0;
+			int x = M[i][j];
+			do {
+				++d_m;
+				x /= 10;
+			} while (x > 0);
+			
+			for (int s = d_m; s< w[j]; ++s)
+				std::cout << ' ';
+				std::cout << M[i][j];
+				std::cout << ' '; 
 		}
-		std::cout << "\n";
+		std::cout << std::endl;
 	}
-	std::cout << "\n" << "\n";
-	for (int i = 0; i < n; ++i){
-		for (int j = 0; j < m; j++){
-			std::cout << "\t" << N[i][j];
+	std::cout << std::endl;
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+
+			int d_m = 0;
+			int x = M[i][j];
+			do {
+				++d_m;
+				x /= 10;
+			} while (x > 0);
+
+			std::cout << M[i][j];
+			for (int s = d_m; s < w[j]; ++s)
+				std::cout << ' ';
+				std::cout << ' ';
 		}
-		std::cout << "\n";
+		std::cout << std::endl;
 	}
+
+	
+	delete[] w;
+	return 0;
 }
